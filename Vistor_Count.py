9bd9ec9c626d0da82cdd164f17ec55a19a5f06c6
ajmclaug1 <74024092+ajmclaug1@users.noreply.github.com>
2021-11-24 15:12:53 +0000

    Update Vistor_Count.py

--- Vistor_Count.py
+++ Vistor_Count.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     Vistors = dytable['Item']
     Vistors = Vistors.get('No_of_Vistors')
     #Increment
-    Vistors += 15
+    Vistors += 1
     #Update Table
     table.update_item(
     Key={
